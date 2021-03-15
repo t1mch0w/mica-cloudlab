3fc7610adfc192c60f24ce26f376ae9182f794a5
root <root@node0.mica-eval.osu-nfs-test-pg0.wisc.cloudlab.us>
2021-03-14 20:14:54 -0600

    Update run_client.sh.

--- build/run_client.sh
+++ build/run_client.sh
@@ -1,4 +1,28 @@
-ips=("128.105.146.7" "128.105.146.5")
+#!/bin/bash
 
-sudo ssh ${ips[0]} "cd /mnt/mica/build;./netbench_client conf_machines_0_EREW_0.5 client0 0 0 conf_workload_0_skewed_0.99_0.01_0.00_1" > cilent0 2>&1 &
-sudo ssh ${ips[1]} "cd /mnt/mica/build;./netbench_client conf_machines_0_EREW_0.5 client1 0 0 conf_workload_0_skewed_0.99_0.01_0.00_1" > client1 2>&1 &
+ips=("128.105.145.217" "128.105.145.220")
+ptime=15
+ttime=75
+
+for dataset in 0 1 2
+do
+for skew in "uniform" "skewed"
+do
+for get in 0.50 0.95
+do
+
+testfile=$(ls conf_workload_${dataset}_${skew}_${get}_*_0.00_1)
+sudo ./netbench_server conf_machines_${dataset}_EREW_0.5 server 0 0 conf_prepopulation_empty > log_server_${testfile} 2>&1 &
+sleep ${ptime}
+
+sudo ssh ${ips[0]} "cd /mnt/mica-cloudlab/build;./netbench_client conf_machines_${dataset}_EREW_0.5 client0 0 0 ${testfile} & sleep ${ttime}; pkill netbench" > ./log_client0_${testfile} 2>&1 &
+sudo ssh ${ips[1]} "cd /mnt/mica-cloudlab/build;./netbench_client conf_machines_${dataset}_EREW_0.5 client1 0 0 ${testfile} & sleep ${ttime}; pkill netbench" > ./log_client1_${testfile} 2>&1 &
+
+sleep $ttime
+sudo pkill netbench
+
+sleep 5
+
+done
+done
+done
